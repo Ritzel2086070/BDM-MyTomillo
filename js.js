c7adec64a334656c8b6053568aa25b671fba0508
Ritzel2086070 <100053852+Ritzel2086070@users.noreply.github.com>
2024-09-02 12:11:01 -0600

    Class avance

--- js.js
+++ js.js
@@ -5,3 +5,24 @@ function toLogin() {
   function toSignIn() {
     window.location.href = 'signIn.html';
   }
+
+
+//ALERTS
+/**/
+function try1(){
+  Swal.fire({
+    title: "¿Está seguro de bloquear el usuario?",
+    showDenyButton: true,
+    showCancelButton: true,
+    confirmButtonText: "Bloquear",
+    denyButtonText: `No bloquear`
+  }).then((result) => {
+    /* Read more about isConfirmed, isDenied below */
+    if (result.isConfirmed) {
+      Swal.fire("Usuario bloqueado", "", "success");
+    } else if (result.isDenied) {
+      Swal.fire("Hubo un error", "", "info");
+    }
+  });
+}
+  
