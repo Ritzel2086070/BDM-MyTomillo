dc5707e9bfdcf04dc020210dd56175843d26ba4e
Ritzel2086070 <100053852+Ritzel2086070@users.noreply.github.com>
2024-09-03 18:36:54 -0600

    class_vAdmin inicio lesson

--- js.js
+++ js.js
@@ -9,19 +9,27 @@ function toLogin() {
 
 //ALERTS
 /**/
-function try1(){
+function LockComent(){
   Swal.fire({
     title: "¿Está seguro de bloquear el usuario?",
-    showDenyButton: true,
+    color: '#86bd7b',
+    background: '#11041F',
+    confirmButtonOutline: 'none',
     showCancelButton: true,
     confirmButtonText: "Bloquear",
-    denyButtonText: `No bloquear`
+    cancelButtonText: "Cancelar",
+    customClass: {
+      confirmButton: 'confirm-button-class',
+      cancelButton: 'cancel-button-class',
+      title: 'title-class',
+      icon: 'icon-class'
+    },
   }).then((result) => {
     /* Read more about isConfirmed, isDenied below */
     if (result.isConfirmed) {
       Swal.fire("Usuario bloqueado", "", "success");
     } else if (result.isDenied) {
-      Swal.fire("Hubo un error", "", "info");
+      Swal.fire("Este mensaje se mantiene", "", "info");
     }
   });
 }
