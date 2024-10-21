7b1d3dc4cd7c533e0c22e84a66f57b23d8b0f466
lilender <96185142+lilender@users.noreply.github.com>
2024-10-21 12:00:33 -0600

    profile pic fix

--- config.php
+++ config.php
@@ -4,8 +4,10 @@ return [
     "database" => [
         "host" => "localhost",
         "user" => "root",
-        "password" => "ungu3554bl3p455byt0m1ll0",
+        "password" => "",
         "dbname" => "MyTomilloCursos",
         "port" => "3306",
     ],
 ];
+
+//ungu3554bl3p455byt0m1ll0
