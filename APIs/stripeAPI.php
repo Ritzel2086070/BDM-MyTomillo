859ff7b7f7dc0580cc346f40c52dbc726c27679a
lilender <96185142+lilender@users.noreply.github.com>
2024-11-28 01:07:50 -0600

    aaa

--- APIs/stripeAPI.php
+++ APIs/stripeAPI.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require __DIR__ . '/../vendor/autoload.php';
+require '/home/ubuntu/MyTomillo/vendor/autoload.php';
 
 require 'secrets.php';
 
