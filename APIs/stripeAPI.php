c63c6fb407efa3ca99841ca96d1ca67b44442941
lilender <96185142+lilender@users.noreply.github.com>
2024-11-28 01:05:53 -0600

    idk

--- APIs/stripeAPI.php
+++ APIs/stripeAPI.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require dirname(__DIR__, 1) . '/vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 require 'secrets.php';
 
