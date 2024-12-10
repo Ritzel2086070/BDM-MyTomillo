cb18368004ce2b3b82bc33f41c25de94409e8b04
lilender <96185142+lilender@users.noreply.github.com>
2024-12-09 23:08:39 -0600

    footer fix?

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,4 +1,14 @@
-<div class="row background-darkpurple d-flex flex-row-reverse">
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+        
+        <div class="row background-darkpurple d-flex flex-row-reverse">
             <div class="col-12 d-flex flex-column justify-content-center align-items-start">
                 <p class="footer">Para acceder a los niveles, se requiere una compra mínima de $10.00 MXN. Los niveles gratuitos o de prueba no se otorgarán sin una compra realizada.</p>
                 <p class="footer">MyTomillo, Inc 2025</p>
